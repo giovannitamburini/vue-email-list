f7c4a0a64057701a93967fa6ee8ae8f973e02c8f
giovannitamburini <giovatambu@gmail.com>
2023-03-22 16:34:30 +0100

    added ex with a function

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ createApp({
             emails: [],
         }
     },
-
+    /*
     mounted() {
 
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail?items=10').then((res) => {
@@ -49,4 +49,21 @@ createApp({
             this.emails.push(res.data.response);
         });
     },
+
+    */
+
+    methods: {
+
+        generateList() {
+
+            for (let i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail?items=10').then((res) => {
+                this.emails.push(res.data.response);
+                });
+
+            }
+        }
+    }
+
 }).mount('#app')
